367fd5602f73a0de0796a195d67b6f3787001c0e
Pablo Trinidad <pabloatp0204@gmail.com>
2018-10-15 14:11:15 -0500

    UVa - 11172

---
+++ UVa/11172/README.md
@@ -0,0 +1,5 @@
+# 11172
+
+* **URL:** [11172](https://uva.onlinejudge.org/index.php?Itemid=8&category=24&option=com_onlinejudge&page=show_problem&problem=2113)
+* **Solved in:**
+    * `C++`
---
+++ UVa/11172/solucion.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a, b, n;
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> a >> b;
+        if (a > b) cout << ">" << endl;
+        else if (a < b) cout << "<" << endl;
+        else cout << "=" << endl;
+    }
+    return 0;
+}
