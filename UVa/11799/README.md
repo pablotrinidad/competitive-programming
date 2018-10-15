c57fc3651ede94835e89124f29092947e4832461
Pablo Trinidad <pabloatp0204@gmail.com>
2018-10-15 15:49:07 -0500

    UVa 11799

---
+++ UVa/11799/README.md
@@ -0,0 +1,5 @@
+# 11799
+
+* **URL:** [11799](https://uva.onlinejudge.org/index.php?Itemid=8&category=24&option=com_onlinejudge&page=show_problem&problem=2899)
+* **Solved in:**
+    * `C++`
---
+++ UVa/11799/solucion.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, a, c, max=0;
+    cin.ignore(); // Ignore number of test cases
+    while (cin >> n) { // Ignore number of students
+        c += 1;
+        for (int i = 0; i < n; ++i) {
+            cin >> a;
+            a > max ? max = a : max = max;
+        }
+        cout << "Case " << c << ": " << max << "\n";
+        max = 0;
+    }
+    return 0;
+}
