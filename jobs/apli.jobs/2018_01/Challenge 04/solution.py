5b9ae714ab608b962c81dc845df2a30c2d6a9134
Pablo Trinidad <pabloatp0204@gmail.com>
2018-10-15 17:59:51 -0500

    Apli - 2018-01 C04 finished

--- jobs/apli.jobs/2018_01/Challenge 04/solution.py
+++ jobs/apli.jobs/2018_01/Challenge 04/solution.py
@@ -1,15 +1,19 @@
-"""
-Write a program that sums the alphabet positions of the characters in a provided string
+"""Sum of chars.
+
+Write a program that sums the alphabet positions of the characters in a provided string.
 Characters are treated case insensitive.
 
 examples:
-sum_chars('ab') returns 3 (a=1, b=2)
-sum_chars('AbCd') returns 10 (a=1, b=2, c=3, d=4)
-sum_chars('Apli') returns 38
+    sum_chars('ab') returns 3 (a=1, b=2)
+    sum_chars('AbCd') returns 10 (a=1, b=2, c=3, d=4)
+    sum_chars('Apli') returns 38
+
 """
 
-def posicion(letter):
-    return ord(letter) - 97
+from string import ascii_letters
+
 
 def sum_chars(s):
-    return None
+    """Sum chars."""
+    s = s.lower()
+    return sum([(i + 1) * s.count(c) for i, c in enumerate(ascii_letters)])
