b813075de0d482de4d6285d838fdae002a1bd50c
Pablo Trinidad <pabloatp0204@gmail.com>
2018-10-15 20:36:52 -0500

    Apli - 2018-02 C03 finished

--- jobs/apli.jobs/2018_02/Challenge 03/solution.py
+++ jobs/apli.jobs/2018_02/Challenge 03/solution.py
@@ -1,4 +1,5 @@
-"""
+"""Maximum square.
+
 Have the function MaximumSquare(strArr) take the strArr parameter being passed which will be a 2D matrix
 of 0 and 1's, and determine the area of the largest square submatrix that contains all 1's.
 A square submatrix is one of equal width and height, and your program should return the area of the
@@ -15,5 +16,42 @@ return the area which is 4.
 You can assume the input will not be empty.
 """
 
-def MaximumSquare(array):
-    return None
+
+def get_max_n(p, l, n_limit):
+    """Expand square trying to obtain the largest sub-matrix within bounds."""
+    n = 1
+    x = p[0]
+    y = p[1]
+    valid = True
+    while valid:
+        for i in range(n):
+            if i == n-1:
+                for j in range(n):
+                    valid = int(l[x + i][y + j]) and valid
+            else:
+                valid = int(l[x + i][y + n - 1]) and valid
+        if n + 1 <= n_limit:
+            n += 1
+        else:
+            break
+
+    return n if valid else n - 1
+
+
+def maximum_square(m):
+    """Maximum square."""
+    s = len(m)
+    n = 1
+    for i in range(s):
+        for j in range(s):
+            if int(m[i][j]) == 1:
+                n_limit = min((s - i), (s - j))
+                max_n = get_max_n((i, j), m, n_limit)  # Create cell expansion
+                if max_n > n:
+                    n = max_n
+    return n ** 2
+
+
+print(
+    maximum_square(["111", "111", "111"])
+)
--- jobs/apli.jobs/2018_02/Challenge 03/test.py
+++ jobs/apli.jobs/2018_02/Challenge 03/test.py
@@ -1,21 +1,21 @@
 import unittest
-from solution import MaximumSquare
+from solution import maximum_square
 
 class TestMaximumSquare(unittest.TestCase):
     def test_01(self):
         strArr = ['10', '01']
-        self.assertEqual(1, MaximumSquare(strArr))
+        self.assertEqual(1, maximum_square(strArr))
 
     def test_02(self):
         strArr = ["10100", "10111", "11111", "10011"]
-        self.assertEqual(4, MaximumSquare(strArr))
+        self.assertEqual(4, maximum_square(strArr))
     def test_03(self):
         strArr = ["111", "111", "111"]
-        self.assertEqual(9, MaximumSquare(strArr))
+        self.assertEqual(9, maximum_square(strArr))
 
     def test_04(self):
         strArr = ["110011", "110011", "100001", "111111", "111111", "111111"]
-        self.assertEqual(9, MaximumSquare(strArr))
+        self.assertEqual(9, maximum_square(strArr))
 
 
 if __name__ == '__main__':
