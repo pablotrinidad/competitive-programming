2fc2d8467a6452de72e856811d4ae8c594d283b2
Pablo Trinidad <pabloatp0204@gmail.com>
2018-12-01 23:42:42 -0600

    HackerRank > Algorithms > Warmup > Repeated String

--- hackerrank/algorithms/warmup/Repeated String/Solution.hs
+++ hackerrank/algorithms/warmup/Repeated String/Solution.hs
@@ -2,13 +2,20 @@ module Main where
 
 import System.IO
 
+asInS :: [Char] -> Int -> Int
+asInS [] _ = 0
+asInS _ 0 = 0
+asInS (x:xs) n
+    | x == 'a' = 1 + (asInS xs (pred n))
+    | otherwise = asInS xs (pred n)
+
 
 main :: IO()
 main = do
-    s' <- getLine
+    s <- getLine
     n' <- getLine
     let n = (read :: String -> Int) $ n'
-    let s = take n . concat . repeat $ s'
-    let r = foldl (\acc x -> if x == 'a' then succ acc else acc) 0 s
+    let l = length s
+    let r = (n `div` l) * (asInS s l) + (asInS s (n `mod` l))
 
     print r
